43eabedab4d7cf9d70846b75af2217f65fcef41d
Joshua Monahan <123738720+jmonahanATU@users.noreply.github.com>
2024-11-28 11:12:27 +0000

    refactor Tab 3 page to implement Alerts and Toasts functionality

--- ui-components-demo/src/app/tab3/tab3.page.ts
+++ ui-components-demo/src/app/tab3/tab3.page.ts
@@ -1,5 +1,19 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, ViewChild } from '@angular/core';
+import { 
+  IonHeader, 
+  IonToolbar, 
+  IonTitle, 
+  IonContent,
+  IonButton,
+  IonModal,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonButtons,
+  IonIcon,
+  AlertController,
+  ToastController
+} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
@@ -7,8 +21,70 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [
+    IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent, 
+    ExploreContainerComponent,
+    IonButton,
+    IonModal,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonButtons,
+    IonIcon
+  ],
 })
 export class Tab3Page {
-  constructor() {}
+  @ViewChild(IonModal) modal!: IonModal;
+
+  constructor(
+    private alertController: AlertController,
+    private toastController: ToastController
+  ) {}
+
+  dismissModal() {
+    this.modal.dismiss();
+  }
+
+  async showMultiActionAlert() {
+    const alert = await this.alertController.create({
+      header: 'Confirm Action',
+      message: 'Are you sure you want to proceed?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => this.showToast('Action cancelled')
+        },
+        {
+          text: 'Delete',
+          cssClass: 'danger',
+          handler: () => this.showToast('Item deleted')
+        },
+        {
+          text: 'Save',
+          cssClass: 'primary',
+          handler: () => this.showToast('Item saved')
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async showToast(message: string, position: 'top' | 'middle' | 'bottom' = 'bottom') {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: position,
+      buttons: [
+        {
+          text: 'Close',
+          role: 'cancel'
+        }
+      ]
+    });
+    await toast.present();
+  }
 }
